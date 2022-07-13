7b218e57a5677ac1cfd2bcf32f1a4a004d929b64
LewisPearsonGitHub <lewiscpearson@gmail.com>
2022-07-13 15:49:20 +0100

    Spring project created

---
+++ src/main/java/com/qa/may/QaProject2FootballSquadBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.qa.may;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaProject2FootballSquadBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaProject2FootballSquadBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/may/QaProject2FootballSquadBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.may;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaProject2FootballSquadBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
